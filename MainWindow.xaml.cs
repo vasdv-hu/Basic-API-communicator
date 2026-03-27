1ccefbea95c3797ceed70eb3d415f864729a328a
repoluhun <vasdavidv@icloud.com>
2026-03-27 13:53:55 +0100

    v0.2.0 (new features and fixes)
    Added loading animation, fixed folder structure, switched to DataContext Binding instead of manually setting text, shortened a couple functions and made them more efficient, fixed (and expanded) caching, added the ability to click on ID to copy it

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,16 @@
-﻿using System.Diagnostics;
+﻿using APIBullshit.ViewModels;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using WpfAnimatedGif;
 
 namespace APIBullshit
 {
-    public class ConfigFile(string URL)
-    {
-        public string URL { get; } = URL;
-    }
-    public class Cat(string id, List<string> tags, DateTime created_at, string url, string mimetype)
+    public class CatResponse(string id, List<string> tags, DateTime created_at, string url, string mimetype)
     {
         public string id { get; } = id;
         public List<string> tags { get; } = tags;
@@ -20,9 +18,13 @@ namespace APIBullshit
         public string url { get; } = url;
         public string mimetype { get; } = mimetype;
     }
+    public class ConfigFile(string URL)
+    {
+        public string URL { get; } = URL;
+    }
     public partial class MainWindow : Window
     {
-
+        BitmapImage throbber = new BitmapImage();
         private string LogPath = String.Empty;
         private void ValidateDirectory(string directoryPath){
             if (!Directory.Exists(directoryPath))
@@ -35,7 +37,7 @@ namespace APIBullshit
                 string rawJSON = File.ReadAllText("config.json");
                 ConfigFile? config = JsonSerializer.Deserialize<ConfigFile>(rawJSON);
 
-                return config.URL;
+                return config!.URL;
             }
             catch (Exception e){
                 WriteToLog(e);
@@ -48,13 +50,20 @@ namespace APIBullshit
                 return configDefault.URL;
             }
         }
-        private static HttpClient jsonClient = new HttpClient();
-        private static HttpClient imgClient = new HttpClient();
+        private static HttpClient jsonClient = new HttpClient()
+        {
+            Timeout = TimeSpan.FromSeconds(10)
+        };
+        private static HttpClient imgClient = new HttpClient() 
+        {
+            Timeout = TimeSpan.FromSeconds(10)
+        };
         string timeNow()
         {
             return DateTime.Now.ToString("yyyy-mm-dd-HH-mm-ss"); 
         }
-        private Cat? currentCat;
+        private CatViewModel? currentCatView;
+        
 
         private void CreateLogFile()
         {
@@ -76,14 +85,52 @@ namespace APIBullshit
             File.AppendAllText(LogPath, $"{timeNow()}|{logText}|{extraData}\n");
         }
 
-        private void CacheKitty(Cat cacheableCat)
+        private void CacheKitty(CatViewModel cacheableCat)
         {
             ValidateDirectory("cache");
 
-            string rawJSON = JsonSerializer.Serialize<Cat>(cacheableCat);
-            File.WriteAllText($"cache/{cacheableCat.id}.json", rawJSON);
+            string rawJSON = JsonSerializer.Serialize<CatViewModel>(cacheableCat);
+            File.WriteAllText($"cache/{cacheableCat.ID}.json", rawJSON);
         }
+        private void TrimCache()
+        {
+            WriteToLog(extraData: "Trying to trim cache");
 
+            try
+            {
+                string[] cachedImages = Directory.GetFiles("cache/imgs");
+
+                if (cachedImages.Count() >= 30)
+                {
+                    var firstTenFiles = cachedImages.Take(10);
+                    foreach (var file in firstTenFiles)
+                    {
+                        File.Delete($"cache/{Path.GetFileNameWithoutExtension(file)}.json");
+                        File.Delete(file);
+                    }
+                    WriteToLog(extraData: "Deleted 10 oldest cached imgs and json files");
+                }
+            }
+            catch (Exception e)
+            {
+                WriteToLog(e, extraData: "Trimming cache failed");
+            }
+        }
+        private string ListToString(List<string> listInput)
+        {
+            string listInString = string.Empty;
+
+            for(int i = 0; i < listInput.Count; i++)
+            {
+                WriteToLog(extraData: $"Tag {i} out of {listInput.Count}");
+                bool isLastItem = i == (listInput.Count - 1);
+                string lastChar = isLastItem ? "" : ",";
+
+                listInString += $"{listInput[i]}{lastChar}";
+            }
+
+            return listInString;
+        }
 
         public MainWindow()
         {
@@ -92,11 +139,17 @@ namespace APIBullshit
                 CreateLogFile();
                 InitializeComponent();
 
+                DataContext = currentCatView;
+
                 Uri addressUri = new Uri(APIURL());
                 jsonClient.BaseAddress = addressUri;
                 imgClient.BaseAddress = addressUri;
                 jsonClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 imgClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("image/*"));
+
+                throbber.BeginInit();
+                throbber.UriSource = new Uri("pack://application:,,,/Images/Spinner.gif");
+                throbber.EndInit();
             }
             catch(Exception e)
             {
@@ -105,61 +158,61 @@ namespace APIBullshit
         }
         private async Task GetAPIResponse()
         {
-            using HttpResponseMessage response = await jsonClient.GetAsync("cat?json=true");
+            HttpResponseMessage response = await jsonClient.GetAsync("cat?json=true");
             var jsonResponse = await response.Content.ReadAsStringAsync();
             WriteToLog(extraData: $"response: {jsonResponse}");
 
-            Cat? kittySerialised = JsonSerializer.Deserialize<Cat>(jsonResponse);
-            bool serialSuccess = kittySerialised != null;
-            WriteToLog(extraData: $"Kitty is serialised : {serialSuccess}");
+            CatResponse? kittyDeserialised = JsonSerializer.Deserialize<CatResponse>(jsonResponse);
+            bool serialSuccess = kittyDeserialised != null;
+            WriteToLog(extraData: $"Kitty is deserialised : {serialSuccess}");
 
             if (!serialSuccess)
                 return;
+            else
+                WriteToLog(extraData: "Aborting GetAPIResponse due to Deserialize fail");
 
-            CacheKitty(kittySerialised!);
+                CatViewModel catTranslate = new CatViewModel()
+                {
+                    ID = kittyDeserialised!.id,
+                    URL = kittyDeserialised.url,
+                    Created_At = kittyDeserialised.created_at,
+                    MimeType = kittyDeserialised.mimetype,
+                    Tags = ListToString(kittyDeserialised.tags)
+                };
 
-            id_TextBox.Text = kittySerialised.id;
-
-            string tagsInLine = string.Empty;
-            foreach(string tag in kittySerialised.tags)
-            {
-                tagsInLine += $"{tag},";
-            }
-            tags_TextBox.Text = tagsInLine;
-
-            created_at_TextBox.Text = kittySerialised.created_at.ToString();
-            mimetype_TextBox.Text = kittySerialised.mimetype;
+            currentCatView = catTranslate;
+            CacheKitty(catTranslate);
 
             ValidateDirectory("cache/imgs");
-            var cachedimgs = Directory.GetFiles("cache/imgs");
-            if (cachedimgs.Count() > 30)
-            {
-                for(int i = 0; i >= 10; i++)
-                {
-                    File.Delete(cachedimgs[i]);
-                }
-            }
+            TrimCache();
 
-            string pathToImg = $"cache/imgs/{kittySerialised.id}.png";
+            string pathToImg = $"cache/imgs/{catTranslate.ID}.png";
+
             if (!File.Exists(pathToImg))
             {
-                byte[] fileBytes = await imgClient.GetByteArrayAsync(kittySerialised.url);
+                WriteToLog(extraData: $"Saving image to {pathToImg}");
+                byte[] fileBytes = await imgClient.GetByteArrayAsync(catTranslate.URL);
                 await File.WriteAllBytesAsync(pathToImg, fileBytes);
             }
+            else
+                WriteToLog(extraData: $"{pathToImg} already exists");
 
-            BitmapImage bit = new BitmapImage();
-            bit.BeginInit(); 
-            bit.CacheOption = BitmapCacheOption.OnLoad;
-            bit.UriSource = new Uri(Path.GetFullPath(pathToImg), UriKind.Absolute);
-            bit.EndInit();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.UriSource = new Uri(pathToImg, UriKind.Relative);
+            bitmapImage.EndInit();
 
-            cat_Image.Source = bit;
+            ImageBehavior.SetAnimatedSource(cat_Image, null);
+
+            cat_Image.Source = bitmapImage;
+            DataContext = currentCatView;
             WriteToLog(extraData: "Data loaded into UI");
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            // TODO : call function that changes Image source to gif
+            ImageBehavior.SetAnimatedSource(cat_Image, throbber);
 
             WriteToLog(extraData: "Calling on API");
             await GetAPIResponse();
---
+++ ViewModels/CatViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace APIBullshit.ViewModels
+{
+    public class CatViewModel : INotifyPropertyChanged
+    {
+        private string _id;
+        private string _tags = "No Tags";
+        private DateTime _created_at;
+        private string _url;
+        private string _mimetype;
+
+        public string ID
+        {
+            get => _id;
+            set { _id = value; OnPropertyChanged(); }
+        }
+        public string Tags
+        {
+            get
+            {
+                if (_tags.Length > 0)
+                    return _tags;
+                else
+                    return _tags = "No Tags";
+            }
+            set { _tags = value; OnPropertyChanged(); }
+        }
+        public DateTime Created_At
+        {
+            get => _created_at;
+            set { _created_at = value; OnPropertyChanged(); }
+        }
+        public string URL
+        {
+            get => _url;
+            set { _url = value; OnPropertyChanged(); }
+        }
+        public string MimeType
+        {
+            get => _mimetype;
+            set { _mimetype = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
